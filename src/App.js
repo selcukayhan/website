1221f6f10ae2998941300507d1bb103e03c81d5d
selcukayhan <selcukayhann@hotmail.com>
2019-04-09 09:25:24 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ class App extends Component {
 
   getResumeData(){
     $.ajax({
-      url:'https://selcukayhan.github.io/website/docs/resumeData.json',
+      url:'/resumeData.json',
       dataType:'json',
       cache: false,
       success: function(data){
