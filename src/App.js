72de5ae239735a2cac8cbf1de03269ef5b480a9e
selcukayhan <selcukayhann@hotmail.com>
2019-04-10 15:33:27 +0300

    resumedata moved

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ class App extends Component {
 
   getResumeData(){
     $.ajax({
-      url:'/images/resumeData.json',
+      url:'/resumeData.json',
       dataType:'json',
       cache: false,
       success: function(data){
