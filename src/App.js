af21fe4cc2a76beb4ac95b9e69ff0f8b466fd241
selcukayhan <selcukayhann@hotmail.com>
2019-04-10 15:37:27 +0300

    public url changed

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ class App extends Component {
 
   getResumeData(){
     $.ajax({
-      url:'/resumeData.json',
+      url:'%PUBLIC_URL%/resumeData.json',
       dataType:'json',
       cache: false,
       success: function(data){
